e27f6c29062efcad074bfa48d9da7aa677e1fc24
Alex <alex.wayne.dev@gmail.com>
2024-02-24 03:27:22 -0700

    Added introduction to learn section

--- MoodTracker/Managers/LearnManager.swift
+++ MoodTracker/Managers/LearnManager.swift
@@ -19,27 +19,28 @@ struct LearnItem {
 
 class LearnManager {
     static let items = [
-        LearnItem(id: 0, name: "What is DBT?", completed: false, image: nil, filename: "What is DBT?"),
-        LearnItem(id: 1, name: "Be Mindful", completed: false, image: nil, filename: "Be-Mindful"),
-        LearnItem(id: 2, name: "Observing Thoughts", completed: false, image: nil, filename: "Observing Thoughts"),
-        LearnItem(id: 3, name: "Describing Emotions", completed: false, image: nil, filename: "Describing Emotions"),
-        LearnItem(id: 4, name: "Participating Fully", completed: false, image: nil, filename: "Participating Fully"),
-        LearnItem(id: 5, name: "Mindful Breathing", completed: false, image: nil, filename: "Mindful Breathing"),
-        LearnItem(id: 6, name: "Mindful Walking", completed: false, image: nil, filename: "Mindful Walking"),
-        LearnItem(id: 7, name: "Wise Mind", completed: false, image: UIImage(named: "wise-mind"), filename: "Wise-Mind"),
-        LearnItem(id: 8, name: "Non-judgmental Stance", completed: false, image: nil, filename: "Non-judgmental Stance"),
-        LearnItem(id: 9, name: "Effectiveness", completed: false, image: nil, filename: "Effectiveness"),
-        LearnItem(id: 10, name: "Mindfulness in Daily Life", completed: false, image: nil, filename: "Mindfulness in Daily Life"),
+        LearnItem(id: 0, name: "Introduction", completed: false, image: nil, filename: "What is DBT?"),
+        LearnItem(id: 1, name: "Setting Good Goals", completed: false, image: nil, filename: "Setting Good Goals"),
+        LearnItem(id: 2, name: "Be Mindful", completed: false, image: nil, filename: "Be-Mindful"),
+        LearnItem(id: 3, name: "Observing Thoughts", completed: false, image: nil, filename: "Observing Thoughts"),
+        LearnItem(id: 4, name: "Describing Emotions", completed: false, image: nil, filename: "Describing Emotions"),
+        LearnItem(id: 5, name: "Participating Fully", completed: false, image: nil, filename: "Participating Fully"),
+        LearnItem(id: 6, name: "Mindful Breathing", completed: false, image: nil, filename: "Mindful Breathing"),
+        LearnItem(id: 7, name: "Mindful Walking", completed: false, image: nil, filename: "Mindful Walking"),
+        LearnItem(id: 8, name: "Wise Mind", completed: false, image: UIImage(named: "wise-mind"), filename: "Wise-Mind"),
+        LearnItem(id: 9, name: "Non-judgmental Stance", completed: false, image: nil, filename: "Non-judgmental Stance"),
+        LearnItem(id: 10, name: "Effectiveness", completed: false, image: nil, filename: "Effectiveness"),
+        LearnItem(id: 11, name: "Mindfulness in Daily Life", completed: false, image: nil, filename: "Mindfulness in Daily Life"),
         
-        LearnItem(id: 11, name: "Understanding Distress Tolerance", completed: false, image: nil, filename: "Understanding Distress Tolerance"),
-        LearnItem(id: 12, name: "Radical Acceptance", completed: false, image: UIImage(named: "circles-of-control"), filename: "Radical-Acceptance"),
-        LearnItem(id: 13, name: "Window of Tolerance", completed: false, image: UIImage(named: "window-of-tolerance"), filename: "Window-of-Tolerance"),
-        LearnItem(id: 14, name: "Pain vs. Suffering", completed: false, image: nil, filename: "Pain-vs-Suffering"),
-        LearnItem(id: 15, name: "Urge Surfing", completed: false, image: UIImage(named: "urge-surfing"), filename: "Urge-Surfing"),
+        LearnItem(id: 12, name: "Understanding Distress Tolerance", completed: false, image: nil, filename: "Understanding Distress Tolerance"),
+        LearnItem(id: 13, name: "Radical Acceptance", completed: false, image: UIImage(named: "circles-of-control"), filename: "Radical Acceptance"),
+        LearnItem(id: 14, name: "Window of Tolerance", completed: false, image: UIImage(named: "window-of-tolerance"), filename: "Window-of-Tolerance"),
+        LearnItem(id: 15, name: "Pain vs. Suffering", completed: false, image: nil, filename: "Pain-vs-Suffering"),
+        LearnItem(id: 16, name: "Urge Surfing", completed: false, image: UIImage(named: "urge-surfing"), filename: "Urge-Surfing"),
         
-        LearnItem(id: 16, name: "GIVE", completed: false, image: nil, filename: "GIVE"),
-        LearnItem(id: 17, name: "FAST", completed: false, image: nil, filename: "FAST"),
-        LearnItem(id: 18, name: "Attending to Relationships", completed: false, image: nil, filename: "Attend-To-Relationships"),
+        LearnItem(id: 17, name: "GIVE", completed: false, image: nil, filename: "GIVE"),
+        LearnItem(id: 18, name: "FAST", completed: false, image: nil, filename: "FAST"),
+        LearnItem(id: 19, name: "Attending to Relationships", completed: false, image: nil, filename: "Attend-To-Relationships"),
     ]
     
     static func getContent(item: LearnItem, completion: (_ contents: String) -> Void) {
