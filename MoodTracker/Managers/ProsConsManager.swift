ebc05a26fd0a25688b8350f2c2a514d899158f77
Alex <alex.wayne.dev@gmail.com>
2024-02-24 03:28:08 -0700

    Pros & Cons Manager

--- MoodTracker/Managers/ProsConsManager.swift
+++ MoodTracker/Managers/ProsConsManager.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class ProsConsManager {
+    
+    static func setup(_ items: [ProsCons]) {
+        AuthManager.user?.prosCons = items
+    }
+    
+    static func new(item: ProsCons) {
+        AuthManager.user?.prosCons.append(item)
+    }
+    
+    static func updateTitle(id: String, title: String) -> Bool {
+        guard let user = AuthManager.user else {
+            return false
+        }
+        
+        guard let index = user.prosCons.firstIndex(where: { $0.id == id}) else {
+            return false
+        }
+        
+        AuthManager.user?.prosCons[index].title = title
+        return true
+    }
+}
